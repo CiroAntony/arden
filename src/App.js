6a522769c323b60d315836c51b7ad7fd488f77ac
CiroAntony <vasquezciro654@gmail.com>
2022-02-07 15:45:21 -0500

    Se modifico el header en app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import banner from './assets/banner.jpg';
 function App() {
   return (
     <div>
-    <header className="header">
-      
+    <header className="main-header">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">Navbar</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
