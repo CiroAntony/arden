6b13f19359cf28d7336a875abc23758c60d9655e
CiroAntony <vasquezciro654@gmail.com>
2022-02-07 23:18:04 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     <header className="header">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/"><img src={logo} alt="" /></a>
+    <a className="navbar-brand" href="/"><img src={logo} alt="" /><span className="LogoName">ARDEN</span><p className="sublogo">EMPRESA CONSTRUCTORA</p></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <ul className="navbar-nav mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">INICIO</a>
         </li>
@@ -24,9 +24,10 @@ function App() {
             SERVICIOS
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
+            <li><a className="dropdown-item" href="/">CONSTRUCCIÓN DE HOTELES</a></li>
+            <li><a className="dropdown-item" href="/">CONSTRUCCIÓN DE EDIFICIOS MULTIFAMILIARES</a></li>
+            <li><a className="dropdown-item" href="/">CONSTRUCCIÓN DE VIVIENDAS</a></li>
+            <li><a className="dropdown-item" href="/">GALERÍAS COMERCIALES</a></li>
           </ul>
         </li>
         <li className="nav-item">
